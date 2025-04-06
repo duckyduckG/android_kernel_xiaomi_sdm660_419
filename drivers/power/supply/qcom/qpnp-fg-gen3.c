c14a8f5e2a86270ea96cec57786e0edfe8d8625a
Manish4586 <manish.n.manish45@gmail.com>
2025-04-06 05:35:52 +0500

    drivers: power: Properly import missing xiaomi changes * In some cases after a kernel panic, battery temperature gets stucked at 40`c or more.   To fix this issue, we needed to reboot the device from fastboot.
    Device: Mi A2/6X
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -616,7 +616,10 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 			BATT_INFO_BATT_TEMP_LSB(fg), rc);
 		return rc;
 	}
-
+#ifdef CONFIG_MACH_LONGCHEER
+		pr_err("addr=0x%04x,buf1=%04x buf0=%04x\n",
+			BATT_INFO_BATT_TEMP_LSB(fg),buf[1],buf[0]);
+#endif
 	temp = ((buf[1] & BATT_TEMP_MSB_MASK) << 8) |
 		(buf[0] & BATT_TEMP_LSB_MASK);
 	temp = DIV_ROUND_CLOSEST(temp, 4);
@@ -624,6 +627,7 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 	/* Value is in Kelvin; Convert it to deciDegC */
 	temp = (temp - 273) * 10;
 #ifdef CONFIG_MACH_LONGCHEER
+		pr_err("LCT TEMP=%d\n",temp);
 #ifdef CONFIG_MACH_XIAOMI_TULIP
 	if (temp < -40) {
 		switch (temp) {

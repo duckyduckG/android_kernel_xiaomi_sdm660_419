6cac1cc0f623894ce4f6908f1999c5556d128a01
duckyduckg <duckyduckg65@gmail.com>
2025-05-05 01:43:07 +0500

    drivers/devfreq/adreno_gov: enable adreno boost by default * Adreno gpu in sdm660 is slow and cant boost in time for GUI, which leads to bad stutters, this is not proper fix but works. user may disable it in kernel manager app.
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -53,7 +53,7 @@ static DEFINE_SPINLOCK(suspend_lock);
 #define TAG "msm_adreno_tz: "
 
 #ifdef CONFIG_ADRENOBOOST
-static unsigned int adrenoboost = 0;
+static unsigned int adrenoboost = 3;
 #endif
 static u64 suspend_time;
 static u64 suspend_start;
